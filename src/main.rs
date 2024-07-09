b87538d25df9da94c80d981ecdb562c618fa9140
Monik Raj Behera <bmonikraj@gmail.com>
2024-07-09 06:37:49 +0530

    adding changes for the log

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
+
 mod inbound;
 mod model;
 mod outbound;
 mod service;
 
+use std::io::Write;
+use chrono::Local;
+use env_logger::Builder;
+
 fn main() {
-    println!("Hello, world!");
+
+    Builder::new()
+        .format(|buf, record| {
+            writeln!(buf,
+                "{} [{}] - {}",
+                Local::now().format("%Y-%m-%dT%H:%M:%S"),
+                record.level(),
+                record.args()
+            )
+        })
+        .filter(None, log::LevelFilter::Debug)
+        .init();
+
+    log::info!("Starting rust web server ...")
 }
